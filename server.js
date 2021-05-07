846ddf3262e98410365e2a25cb87f15583a0a4f6
Paulo Ricardo Acílio Chimelo <prac0402@gmail.com>
2021-05-06 21:51:39 -0300

    a

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const http = require('https');
 
 const app = require('./app')
-window.location.href = "/index.html"
+//window.location.href = "/index.html"
 const port = process.env.PORT || 3000;
 
 const server = http.createServer(app);
